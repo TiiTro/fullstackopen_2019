fa2ff5b86ad8981992e1322ba637353be2002a7e
TiiTro <tiiatrogen@gmail.com>
2019-05-19 21:52:22 +0300

    Assignments 1.3-1.5

--- Osa_1/kurssitiedot/src/index.js
+++ Osa_1/kurssitiedot/src/index.js
@@ -2,56 +2,69 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Part = (props) => {
-    return(
+    console.log(props)
+    return (
         <div>
-            <p>{props.part} {props.exercises}</p>
-        </div>
+            <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+            <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+            <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+        </div> 
+    )    
+}
+
+const Header = (props) => {
+    console.log(props);
+    return (
+        <>
+            <h1>{props.course}</h1>
+        </>
     )
 }
 
-const Header = (props) => (
-    <>
-        <h1>{props.course}</h1>
-    </>
-)
+const Content = (props) => {
+    console.log(props)
+    return (
+        <>
+            <Part parts={props.parts}/>
+        </>
+    )
+}
 
-const Content = (props) => (
-    <>
-        <Part part={props.part1} exercises={props.exercises1}/>
-        <Part part={props.part2} exercises={props.exercises2}/>
-        <Part part={props.part3} exercises={props.exercises3}/>
-    </>
-)
-
-const Total = (props) => (
-    <>
-        <p>{props.totalExercises}</p>
-    </>
-)
+const Total = (props) => {
+    console.log(props)
+    return(
+        <>
+            <p>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+        </>
+    )
+}
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
-
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+            {
+                name: 'Reactin perusteet',
+                exercises: 10
+            },
+            {
+                name: 'Tiedonvälitys propseilla',
+                exercises: 7
+            },
+            {
+                name: 'Komponenttien tila',
+                exercises: 14
+            }
+        ]
+    }
+    
     return(
         <div>
-            <Header course={course} />
-            <Content 
-                part1={part1}
-                part2={part2}
-                part3={part3}
-                exercises1={exercises1}
-                exercises2={exercises2}
-                exercises3={exercises3}/>
-            <Total totalExercises={exercises1 + exercises2 + exercises3}/>
+            <Header course={course.name} />
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
         </div>
     )
-    
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
