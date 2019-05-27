13dcde8a06b972daa75fae50e222cb73ac40f359
TiiTro <tiiatrogen@gmail.com>
2019-05-27 20:19:53 +0300

    Assignments 1.7-1.11

--- Osa_1/unicafe/src/index.js
+++ Osa_1/unicafe/src/index.js
@@ -1,32 +1,51 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
 const Button = (props) => {
   return (
     <button onClick={props.onClick}>{props.text}</button>
   )
 }
 
-// const Statistics = (props) => {
-//   return(
-//     <div>
-//       <p>{props.text} {props.count}</p>
-//     </div>
-//   ) 
-// }
+const Statistic = (props) => {
+  return(
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
 
-const Statistics = ({countGood, countNeutral, countBad}) => {
-  console.log(countGood);
-  console.log(countNeutral);
-  console.log(countBad);
+const Statistics = ({countGood, countNeutral, countBad, countAll}) => {
+
+  const sum = countAll.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+
+  const average = (sum/countAll.length).toFixed(1);
+
+  const positiveFeedback = (countGood/countAll.length * 100).toFixed(1) + '%';
+
+  if(countAll.length === 0) {
+    return(
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
 
   return(
-    <div>
-      <p>good {countGood}</p>
-      <p>neutral {countNeutral}</p>
-      <p>bad {countBad}</p>
-      <p>all {countGood + countNeutral + countBad}</p>        
-    </div>
+    <table>
+      <tbody>
+        <Statistic text='good' value={countGood}/>
+        <Statistic text='neutral' value={countNeutral}/>
+        <Statistic text='bad' value={countBad}/>
+        <Statistic text='all' value={countAll.length}/>
+        <Statistic text='average' value={average}/>
+        <Statistic text='positive' value={positiveFeedback}/>
+      </tbody>
+    </table>
   ) 
 }
 
@@ -35,24 +54,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([]);
 
-  console.log(good);
-  console.log(neutral);
-  console.log(bad);
+  const handleGoodClick = () => {
+    setAll(all.concat(1));
+    setGood(good + 1);
+  }
+
+  const handleNeutralClick = () => {
+    setAll(all.concat(0));
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadClick = () => {
+    setAll(all.concat(-1));
+    setBad(bad + 1);
+  }
+
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button text={'good'} onClick={() => setGood(good +1)}/>
-      <Button text={'neutral'} onClick={() => setNeutral(neutral +1)}/>
-      <Button text={'bad'} onClick={() => setBad(bad +1)}/>
+      <Button text={'good'} onClick={handleGoodClick}/>
+      <Button text={'neutral'} onClick={handleNeutralClick}/>
+      <Button text={'bad'} onClick={handleBadClick}/>
       <h1>statistics</h1>
       <Statistics 
         countGood={good}
         countNeutral={neutral}
-        countBad={bad}/>
-       {/* <Statistics text={'good'} count={good}/>
-       <Statistics text={'neutral'} count={neutral}/>
-       <Statistics text={'bad'} count={bad}/> */}
+        countBad={bad}
+        countAll={all}/>
     </div>
   )
 }
